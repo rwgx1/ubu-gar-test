9bcc4059015f9149d34d74b255aa90292985656c
First Last <first,last@gmail.com>
2025-11-07 15:09:02 +0000

    update

--- setup-git.sh
+++ setup-git.sh
@@ -38,8 +38,8 @@ echo "--------------------------------------------------------------------------
 echo "this is project https://github.com/rwgx1/"$project
 echo "-----------------------------------------------------------------------------"
 git config --global pull.rebase false
-git config --global user.name "Richard Griffith"
-git config --global user.email "rgriff29@gmail.com"
+git config --global user.name "First Last"
+git config --global user.email "first,last@gmail.com"
 #sudo git config --system core.editor nano
 #git config --global credential.helper cache
 #git config --global credential.helper 'cache --timeout=32000'
