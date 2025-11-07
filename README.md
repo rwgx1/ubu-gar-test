55f2b3e2dba8f1998fe7bcb4207e113394b9ff62
Richard Griffith <rgriff29@gmail.com>
2025-11-07 15:00:21 +0000

    update

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # ubu-gar-test
 quick test
+
+Make Ubuntu a little lihe Garuda
---
+++ up.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+set -uo pipefail  # Do not use set -e, we want to continue on error
+##################################################################################################################
+# Author    : Erik Dubois
+# Website   : https://www.erikdubois.be
+# Youtube   : https://youtube.com/erikdubois
+# Github    : https://github.com/erikdubois
+# Github    : https://github.com/kirodubes
+# Github    : https://github.com/buildra
+# SF        : https://sourceforge.net/projects/kiro/files/
+##################################################################################################################
+#
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
+#
+##################################################################################################################################
+#tput setaf 0 = black
+#tput setaf 1 = red
+#tput setaf 2 = green
+#tput setaf 3 = yellow
+#tput setaf 4 = dark blue
+#tput setaf 5 = purple
+#tput setaf 6 = cyan
+#tput setaf 7 = gray
+#tput setaf 8 = light blue
+
+#end colors
+#tput sgr0
+##################################################################################################################################
+
+# reset - commit your changes or stash them before you merge
+# git reset --hard - ArcoLinux alias - grh
+
+# reset - go back one commit - all is lost
+# git reset --hard HEAD~1
+
+# remove a file online but keep it locally
+# https://www.baeldung.com/ops/git-remove-file-without-deleting-it
+# git rm --cached file.txt
+
+workdir=$(pwd)
+
+### ./chaotic
+
+#rm $workdir/mirrorlist
+#touch $workdir/mirrorlist
+
+# Below command will backup everything inside the project folder
+git add --all .
+
+# Give a comment to the commit if you want
+echo
+echo "####################################"
+echo "Write your commit comment!"
+echo "####################################"
+echo
+
+input="update"
+
+# Committing to the local repository with a message containing the time details and commit text
+
+git commit -m "$input"
+
+# Push the local files to github
+
+branch=$(git rev-parse --abbrev-ref HEAD)
+git push -u origin "$branch"
+
+echo
+tput setaf 6
+echo "##############################################################"
+echo "###################  $(basename $0) done"
+echo "##############################################################"
+tput sgr0
+echo
